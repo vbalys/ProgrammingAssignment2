48114d1c616b4580ea1bb1a794eee182196b2c57
Vaidas Balys <vbalys@gmail.com>
2015-01-11 14:00:43 +0200

    Added function for creating cached matrix object

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions below are used to store a matrix and cache its inverse.
 
-## Write a short comment describing this function
+## Function creates and returns special object for matrix which is a list 
+## containing functions to
+##  - set the value of matrix
+##  - get the value of matrix
+##  - set the value of inverse
+##  - get the value of inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
+    list(
+        set=set,
+        get=get,
+        setinverse=setinverse,
+        getinverse=getinverse
+    )
 }
 
 
